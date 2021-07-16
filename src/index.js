599681b2896eb3e27101adebd5d883bc431f8713
Irina Paliakova <76568191+irene-paliakova@users.noreply.github.com>
2021-07-16 13:45:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array)
+    return 0;
+  if (array.length == 0)
+    return 0;
+  let min = array[0];
+  array.map(el => min = el < min ? el : min);
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array)
+    return 0;
+  if (array.length == 0)
+    return 0;
+  let max = array[0];
+  array.map(el => max = el > max ? el : max);
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array)
+    return 0;
+  if (array.length == 0)
+    return 0;
+  let sum = array.reduce((s, el) => s + el);
+  return sum / array.length;
 }
